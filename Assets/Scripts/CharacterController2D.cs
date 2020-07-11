373dc3874c691eebc5c9689953adfa6c9946ca7e
Suritama <IchBinDeppert@outlook.com>
2020-07-11 03:01:20 +0200

    sleepyboi can walk now

---
+++ Assets/Scripts/CharacterController2D.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class CharacterController2D : MonoBehaviour
+{
+    [SerializeField, Tooltip("Max speed, in units per second, that the character moves.")]
+    float speed = 9;
+
+    [SerializeField, Tooltip("Acceleration while grounded.")]
+    float walkAcceleration = 75;
+
+    private BoxCollider2D boxCollider;
+
+    private Vector2 velocity;
+
+    private void Awake()
+    {      
+        boxCollider = GetComponent<BoxCollider2D>();
+    }
+
+    private void Update()
+    {
+		transform.Translate(velocity * Time.deltaTime);
+		float moveInputHorizontal = Input.GetAxisRaw("Horizontal");
+		velocity.x = Mathf.MoveTowards(velocity.x, speed * moveInputHorizontal, walkAcceleration * Time.deltaTime);
+		float moveInputVertical = Input.GetAxisRaw("Vertical");
+		velocity.y = Mathf.MoveTowards(velocity.y, speed * moveInputVertical, walkAcceleration * Time.deltaTime);
+    }
+}

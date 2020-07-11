12ea1399add624fcb78f1fc88b6ed6c5c25361a4
Suritama <IchBinDeppert@outlook.com>
2020-07-12 01:20:32 +0200

    tutorial level

---
+++ Assets/Scripts/LetsBeginButton.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class LetsBeginButton : MonoBehaviour
+{
+	public string nextLevel;
+	public Button button;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+         button.GetComponent<Button>().onClick.AddListener(loadNextLevel);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+	
+	void loadNextLevel()
+	{
+			StartCoroutine(LoadYourAsyncScene());
+		
+	}
+
+    IEnumerator LoadYourAsyncScene()
+    {
+        // The Application loads the Scene in the background as the current Scene runs.
+        // This is particularly good for creating loading screens.
+        // You could also load the Scene by using sceneBuildIndex. In this case Scene2 has
+        // a sceneBuildIndex of 1 as shown in Build Settings.
+
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(nextLevel);
+
+        // Wait until the asynchronous scene fully loads
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+	}
+}

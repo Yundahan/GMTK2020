38d474b9adfe0112b71e3a71318278f14d88d3fb
Suritama <IchBinDeppert@outlook.com>
2020-07-12 13:25:02 +0200

    PUSH DA BUTTON

---
+++ Assets/Scripts/PushButton.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PushButton : MonoBehaviour
+
+
+{
+	private SpriteRenderer spriteR;
+	
+	public Sprite buttonPressed;
+	public Sprite buttonUnpressed;
+	public GameObject triggeredObject;
+
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteR = gameObject.GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+	
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		triggeredObject.SendMessage("buttonTriggered");
+		spriteR.sprite = buttonPressed;
+	}
+	
+	void OnTriggerExit2D (Collider2D col)
+	{
+		spriteR.sprite = buttonUnpressed;
+
+	}	
+	
+}

63b3af07c16cbfe20c34e1fe67244b792008f9a7
Suritama <IchBinDeppert@outlook.com>
2020-07-12 12:47:15 +0200

    light functionality

--- Assets/Scripts/LightBehaviour.cs
+++ Assets/Scripts/LightBehaviour.cs
@@ -3,11 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class LightBehaviour : MonoBehaviour
+
+
 {
+	private bool lightswitch = true;
+	private SpriteRenderer spriteR;
+	
+	public Sprite lightVisible;
+	public Sprite lightInvisible;
+
+	
     // Start is called before the first frame update
     void Start()
     {
-        
+        spriteR = gameObject.GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -18,13 +27,27 @@ public class LightBehaviour : MonoBehaviour
 	
 	void OnTriggerEnter2D(Collider2D col)
 	{
-		if(col.gameObject.name == "Sleepyboi")
+		if(col.gameObject.name == "Sleepyboi" && lightswitch)
 		{
 			col.gameObject.SendMessage("WakeUpStill");
 		}
-		else if(col.gameObject.name == "Ghost")
+		else if(col.gameObject.name == "Ghost" && lightswitch)
 		{
 			col.gameObject.SendMessage("Kill");
 		}
 	}
+	
+	void LightsOn()
+	{
+	lightswitch = true;	
+	spriteR.sprite = lightVisible;
+		
+	}
+	
+	void LightsOff()
+	{
+	 lightswitch = false;
+	 spriteR.sprite = lightInvisible;	
+	}	
+	
 }
